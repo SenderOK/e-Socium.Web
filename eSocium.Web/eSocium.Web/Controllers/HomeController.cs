c30522346494d93db6be2f97677939e1f7e7165f
naidyonov <naidyonov@gmail.com>
2012-03-29 21:18:52 +0400

    Создан проект e-Socium

---
+++ eSocium.Web/eSocium.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eSocium.Web.Models.DAL;
+
+namespace eSocium.Web.Controllers
+{
+    /// <summary>
+    /// Главный контроллер сайта, который содержит в себе информационные страницы и ссылки на 
+    /// подпроекты
+    /// </summary>
+    public class HomeController : Controller
+    {
+        UserContext db = new UserContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.User = User.Identity.Name;
+            return View();
+        }
+    }
+}
---
+++ eSocium.Web/eSocium.Web/Models/DAL/UserInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace eSocium.Web.Models.DAL
+{
+    /// <summary>
+    /// Собственный инициализатор базы данных
+    /// В случае, если модель поменялась, то пересоздать базу и добавить в нее некоторые компоненты
+    /// </summary>
+    public class UserInitializer : DropCreateDatabaseIfModelChanges<UserContext>
+    {
+        protected override void Seed(UserContext context)
+        {
+            var users = new List<User>
+            {
+                new User { UserId = 1, Email = "admin@admin.com", Login = "admin", Password = PasswordHash.ComputeHash("admin"), Role = 1 }
+            };
+            users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+
+            var roles = new List<Role>
+            {
+                new Role { RoleId = 1, Name = "Администратор" },
+                new Role { RoleId = 2, Name = "Пользователь" }
+            };
+            roles.ForEach(s => context.Roles.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
