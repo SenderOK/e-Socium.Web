e7c27d04ebebe6bd32c998cd086905038a87bd17
Andrew Bodrow <drinob@gmail.com>
2012-04-13 17:43:26 +0400

    Поправлен баг отображения загрузки вопроса.

--- eSocium.Web/eSocium.Web/Scripts/Open/CreateQuestions.js
+++ eSocium.Web/eSocium.Web/Scripts/Open/CreateQuestions.js
@@ -1,19 +1,31 @@
 ﻿$(document).ready(function () {
-    var Question_Form_Storage;
-    var Question_Mark_Storage;
-    $(hasHeaderCheckbox).change(function () {
-        $(Form).toggle(!this.checked);
-        $('label[for="Form"]').toggle(!this.checked);
-        $(Mark).toggle(!this.checked);
-        $('label[for="Mark"]').toggle(!this.checked);
-        if (this.checked) {
+    var Question_Form_Storage = $(Form).val();
+    var Question_Mark_Storage = $(Mark).val();
+
+    function SetCheckboxState(is_checked) {
+        is_checked = $(hasHeaderCheckbox).is(':checked');
+        $(Form).toggle(!is_checked);
+        $('label[for="Form"]').toggle(!is_checked);
+        $(Mark).toggle(!is_checked);
+        $('label[for="Mark"]').toggle(!is_checked);
+        if (is_checked) {
             Question_Form_Storage = $(Form).val();
             Question_Mark_Storage = $(Mark).val();
-            $(Form).val("NOT NEEDED");
-            $(Mark).val("NOT NEEDED");
+            if ((Question_Form_Storage).trim().length == 0) {
+                $(Form).val("NOT NEEDED");
+            }
+            if ((Question_Mark_Storage).trim().length == 0) {
+                $(Mark).val("NOT NEEDED");
+            }
         } else {
             $(Form).val(Question_Form_Storage);
             $(Mark).val(Question_Mark_Storage);
         }
+    }
+
+    SetCheckboxState();
+
+    $(hasHeaderCheckbox).change(function () {
+        SetCheckboxState();
     });
 });
